d18db0f1be1abbb4e0250b2387956a8762af0ef8
herbayulet <sayaherbayu@gmail.com>
2022-09-10 21:05:48 +0700

    news app Release-1 done

--- components/CardNews.jsx
+++ components/CardNews.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 const CardNews = ({ title, imageUrl, author, date, klik }) => {
     return (
-        <div className="h-auto m-4 border-2 rounded-lg bg-violet-50 w-60">
+        <div className="m-4 border-2 rounded-lg h-[450px] border-teal-400 w-60 backdrop-blur bg-transparent">
             <h1 className="my-2 font-bold text-center text-black">{title}</h1>
-            <Image unoptimized={true} className="w-full h-52" src={imageUrl} alt={author} width={500} height={500} />
+            <Image unoptimized={true} className="absolute w-full h-52" src={imageUrl} alt={author} width={500} height={500} />
             <h2 className="mt-2 text-center text-black ">Author :{author}</h2>
             <p className="text-center text-black font-poppins">Date :{date}</p>
             <div className="flex justify-center pb-2 mt-2 text-white">
